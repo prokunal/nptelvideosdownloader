6b489ca78ab3db630ef33b3be502b0dad8fd08e9
Kunal Kumar <k9801263933@gmail.com>
2021-08-28 05:38:30 -0400

    Update nptel_videos_downloader.py

--- nptel_videos_downloader.py
+++ nptel_videos_downloader.py
@@ -5,6 +5,7 @@ import os,sys,wget,urllib.request,threading
 from bs4 import BeautifulSoup
 from urllib.request import Request, urlopen
 
+
 prefix = "https://nptel.ac.in"
 course_link = sys.argv[1]
 req = Request(course_link)
@@ -20,10 +21,17 @@ for i in temp_links:
   if i.endswith('.mp4') or i.endswith('.MP4') or i.endswith('.mP4') or i.endswith('.Mp4'):
     links.append(prefix+i)
 
+tmp_link = links
+_dir = os.listdir(".")
 
-
-
-l1 = l2 = l3 = l4 = round(len(links)/4)
+for i in range(1,10,1):
+  for i in tmp_link:
+    if os.path.basename(i) in _dir:
+      links.remove(i)
+if len(links) == 0:
+  print("All Videos are Downloaded Already :)")
+  exit(0)
+l1 = round(len(links)/4)
 
 link1 = links[0:l1]
 link2 = links[l1:l1+l1]
@@ -32,7 +40,7 @@ link4 = links[l1+l1+l1:l1+l1+l1+l1]
 
 size_count = 0
 print("Calculating Total size of all files, it may take upto 1 minute...")
-print("Warning: Don't cancel the process, restarting this program will start downloading from beginning.")
+
 for i in links:
   size = urllib.request.urlopen(i)
   size_count = size.length + size_count
@@ -62,7 +70,7 @@ def downloader4():
     for i in link4:
       filename = wget.download(i,bar=bar_progress)
       print(filename + " Downloaded")
-
+print("All Video got Downloaded :)")        
 t1 = threading.Thread(target=downloader1, name='d1')
 t2 = threading.Thread(target=downloader2,name='d2')
 t3 = threading.Thread(target=downloader3, name='d3')
